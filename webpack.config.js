cffa131b9ff6fc02f7029c055e4ed5849a9e9d1f
Augustine <smaji08@gmailcom>
2020-04-01 17:37:13 -0400

    service worker and webpack added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./public/index.js"
+  },
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "[name].bundle.js"
+  },
+  mode: "development",
+  
+  plugins: [
+    new WebpackPwaManifest({
+      name: "Budget app",
+      short_name: "Budgety",
+      description: "An application to track your budget.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      display: "standalone",
+      icons: [{
+        src: path.resolve(__dirname, "public/icons/icon-192x192.png"),
+        size: [192,512],
+      }]
+    })
+  ]
+};
+
+module.exports = config;
