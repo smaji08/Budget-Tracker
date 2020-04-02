b54658385bf001b2bb70a563b824f0ec3f6e4fa2
Augustine <smaji08@gmailcom>
2020-04-02 14:17:20 -0400

    mode changed to production

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
     path: __dirname + "/public/dist",
     filename: "[name].bundle.js"
   },
-  mode: "development",
+  mode: "production",
   
   plugins: [
     new WebpackPwaManifest({
